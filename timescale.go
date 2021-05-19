2be7ea870fe22f4c9234ebf71158a03520d555c6
MarcinWylot <MarcinWylot@users.noreply.github.com>
2021-05-19 18:47:12 +0200

    add test_db function

--- timescale.go
+++ timescale.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
@@ -41,4 +43,16 @@ func timescaleConnect() {
 	if err != nil {
 		log.Fatalf("Unable to connect to database: %v\n", err)
 	}
+}
+
+func test_db() {
+	//run a simple query to check our connection
+	var greeting string
+	err := dbPool.QueryRow(context.Background(), "select 'Hello, Timescale!'").Scan(&greeting)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(greeting)
 }
